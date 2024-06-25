d47590a8e0dd7b2189bf391be29bba2ff6bbee67
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-25 11:35:30 -0400

    Display stocks

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,13 +1,17 @@
-import React from "react";
-import Stock from "./Stock";
+import React from "react"
+import Stock from "./Stock"
 
-function StockContainer() {
+function StockContainer({ stocks }) {
+  const displayedStocks = stocks.map((stock) => {
+    return <Stock key={stock.id} stock={stock} />
+  })
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {displayedStocks}
     </div>
-  );
+  )
 }
 
-export default StockContainer;
+export default StockContainer
